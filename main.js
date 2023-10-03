f194a4e3ec6e4d1d496073d4417f645c353c98f9
ConnerCleland <Chcleland2@gmail.com>
2023-10-03 14:27:38 -0500

    used chatgpt to fix it

--- main.js
+++ main.js
@@ -3,13 +3,19 @@ document.addEventListener("DOMContentLoaded", function () {
     const outputDiv = document.getElementById("output");
 
     fetchDataButton.addEventListener("click", function () {
-        const apiUrl = 'https://v2.jokeapi.dev/joke/Any?safe-mode';
+        const apiUrl = 'https://v2.jokeapi.dev/joke/Any?type=single&safe-mode';
 
         fetch(apiUrl)
             .then((response) => response.json())
             .then((data) => {
-                const jokeSentence = `${data.setup} ${data.delivery}`;
-                outputDiv.innerHTML = `<p>${jokeSentence}</p>`;
+                if (data.type === 'twopart') {
+                    const jokeSentence = `${data.setup} ${data.delivery}`;
+                    outputDiv.innerHTML = `<p>${jokeSentence}</p>`;
+                } else if (data.type === 'single') {
+                    outputDiv.innerHTML = `<p>${data.joke}</p>`;
+                } else {
+                    outputDiv.innerHTML = `<p>Invalid joke type received</p>`;
+                }
             })
             .catch((error) => {
                 console.error("Error fetching data:", error);
